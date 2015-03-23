abf5240899da8c08ca63765d1b382cf4834aea2b
Peter C. Lai <peter.lai2@sbdinc.com>
2015-03-23 14:47:30 -0400

    1. Add support -a, --append for autoappend to existing table name 2. Add support -y, --yes for autodrop/overwrite existing table name    without having to hit 'y' at confirmation prompt 3. Add support --delim for specifying delimiter on command line    (to support calling this script from another and passing    dynamic delimiter, as in BPCS file validators) 4. Add support --qual for specifying qualifier on command line    (to support calling this script from another and passing    dynamic qualifier, as in BPCS file validators) 5. Add support --encoding for specifying encoding on command line     (to support calling this script from another and passing a     dynamic encoding string, as in BPCS file validator) 6. Add support --override, override invalid delimiter characters
     On branch master
     Changes to be committed:
    	modified:   readconfig.py

--- config/readconfig.py
+++ config/readconfig.py
@@ -3,10 +3,16 @@ import os, sys
 
 parser = argparse.ArgumentParser(
     description='ETL Data from Data Source to PostgreSQL target')
+parser.add_argument('-a','--append', action='store_true')
 parser.add_argument('-c', '--config')
 parser.add_argument('-f', '--from', dest='src_data')
 parser.add_argument('-t', '--to', dest='target_table')
 parser.add_argument('-d', '--debug', action='store_true')
+parser.add_argument('-y', '--yes', action='store_true')
+parser.add_argument('--delim', dest='delim')
+parser.add_argument('--qual', dest='qual')
+parser.add_argument('--encoding',dest='encoding')
+parser.add_argument('--override',action='store_true')
 parser.add_argument('src_data_', metavar='SRC_DATA', nargs='?',
     default='')
 parser.add_argument('target_table_', metavar='TARGET_TABLE', nargs='?',
@@ -23,6 +29,7 @@ else:
     config.read([fullconfig, localconfig])
 
 def get_flatfile():
+    errors = 0
     flatfile = {}
 
     # read items in the .ini file
@@ -31,38 +38,61 @@ def get_flatfile():
 
     # try to find a suitable delimiter
     try:
-        delim = ast.literal_eval(flatfile['delimiter'])
+        delim0 = flatfile['delimiter']
+    except KeyError:
+        delim0 = args.delim
+        if delim0 is None and not args.override:
+            raise SystemExit('No Delimited specified in the config file under the [flatfile] section nor with --delim')
+    if delim0 is None:
+        delim = ''
+    else:
+        if delim0[0] == 'u':
+            delim = ast.literal_eval(delim0)
+        elif delim0[0] == '\\':
+            delim = ast.literal_eval("u'"+delim0+"'")
+        else:
+            delim = delim0
+        if args.debug:
+            print "Delim Raw: {}".format(repr(delim0))
+            print "Delim postproc: {}".format(repr(delim))
         if not isinstance(delim, basestring):
             raise SystemExit(
                 'Specified Delimiter is not a string, '
                 'was given {0!r}'.format(delim))
         else:
-            delim_len = len(delim)
-            if delim_len != 1:
+            if len(delim) != 1 and not args.override:
+                print repr(delim)
                 raise SystemExit(
                     'Delimiter must be 1 character wide '
-                    'but is {} wide in config'.format(delim_len))
-        flatfile['delimiter'] = delim
-    except KeyError:
-        raise SystemExit(
-            'Delimiter not specified in config file for section [flatfile]')
+                    'but is {} wide in config'.format(len(delim)))
+    flatfile['delimiter'] = delim
 
     #try to find a suitable qualifier, if specified
     try:
-        qual = ast.literal_eval(flatfile['qualifier'])
+        qual0 = flatfile['qualifier']
+    except KeyError:
+        qual0 = args.qual
+    if qual0 is None:
+        qual = ''
+    else:
+        if qual0[0] == 'u':
+            qual = ast.literal_eval(qual0)
+        elif qual0[0] == '\\':
+            qual = ast.literal_eval("u'"+qual0+"'")
+        else:
+            qual = qual0
         if not isinstance(qual, basestring):
             raise SystemExit(
                 'Specified Qualifier is not a string, '
                 'was given {0!r}'.format(qual))
-        else:
-            qual_len = len(qual)
-            if qual_len != 1:
-                raise SystemExit(
-                    'Qualifier must be 1 character wide '
-                    'but is {} wide in config'.format(qual_len))
-        flatfile['qualifier'] = qual
-    except KeyError:
-        flatfile['qualifier'] = ''
+        if len(qual) > 1:
+            raise SystemExit(
+                'Qualifier must be 1 character wide '
+                'but is {} wide in config'.format(len(qual)))
+        if args.debug:
+            print "Qual 0: {}".format(repr(qual0))
+            print "Qual postproc: {}".format(repr(qual))
+    flatfile['qualifier'] = qual
 
     # try to parse a list of field identifiers with data types
     # syntax is FIELDNAME, ABAP_TYPE
@@ -83,7 +113,11 @@ def get_flatfile():
         raise SystemExit('No fields defined in config to map')
 
     if 'encoding' not in flatfile:
-        flatfile['encoding'] = 'ascii'
+        if args.encoding is None:
+            flatfile['encoding'] = 'ascii'
+        else:
+            flatfile['encoding'] = args.encoding
+
 
     if 'source' not in flatfile:
         if args.src_data:
@@ -168,6 +202,40 @@ def debug_config():
         debug['debug'] = 0
 
     return debug
+
+def yes_config():
+    yes_flag = 0
+    if args.yes:
+        yes_flag = 1
+    return yes_flag
+
+def append_config():
+    append_flag = 0
+    if args.append:
+        append_flag = 1
+    return append_flag
+
+def validator_config():
+    errors = 0
+    validator = {}
+
+    # read items in the .ini file
+    for item in config.items('validator'):
+        validator[item[0]] = item[1]
+    try:
+        fields = [field.strip() for field in
+            flatfile['fields'].split(',')]
+        if len(fields) < 1:
+            raise SystemExit('No fields defined in config to map')
+        fields_ = [field.split() for field in fields]
+        for field in fields_:
+            if len(field) != 2:
+                raise SystemExit(
+                    "Field spec {} must be in format: FIELD TYPE".format(
+                    field))
+        flatfile['fields'] = fields_
+    except KeyError:
+        raise SystemExit('No fields defined in config to map')
 
 
 # Unit tests
